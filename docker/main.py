1e936ef8d0c13d706cc4769286ff6c77f88d7bb9
wilburlua910 <wilburlua910@outlook.com>
2024-10-28 00:08:52 +0800

    Update

--- docker/main.py
+++ docker/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    sucess_msg = "Your Kubernetes deployment is successful!"
+    return render_template("index.html", message=sucess_msg)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5001)
